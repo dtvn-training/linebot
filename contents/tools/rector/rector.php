c01b05a3ea10f309aade256e00839810bc04ee98
unknown <manh.nguyen@dac-datatech.vn>
2024-04-25 12:08:48 +0700

    Rector - setlist

--- contents/tools/rector/rector.php
+++ contents/tools/rector/rector.php
@@ -1,21 +1,28 @@
 <?php
 
-use Rector\Config\RectorConfig;
+use Rector\Config\RectorConfig; 
+use RectorLaravel\Set\LaravelSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 $dir = '/var/www/html/app/';
 $files = [];
 $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
 foreach ($iterator as $file) {
-    if ($file->isFile() && $file->getExtension() === 'php') {
+    // if ($file->isFile() && $file->getExtension() === 'php') {
         $files[] = $file->getPathname();
-    }
+    // }
 }
 return RectorConfig::configure()
+    ->withSets([
+        LaravelSetList::LARAVEL_100
+    ])
+    ->withAttributesSets(symfony: true, doctrine: true)
+    ->withPhpSets(php84: true)
+    ->withFileExtensions(['php', 'phtml', 'js', 'blade.php'])
     ->withRules([
         TypedPropertyFromStrictConstructorRector::class
     ])
     ->withPaths($files)
     ->withPreparedSets(
         deadCode: true,
-        codeQuality: true
+        codeQuality: true,
     );
