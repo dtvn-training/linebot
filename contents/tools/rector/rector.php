ba92a3027d460ac7babbcf905fb6bc0388b6dd30
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-05-02 01:29:56 +0700

    update Rector and Actions.yml files

--- contents/tools/rector/rector.php
+++ contents/tools/rector/rector.php
@@ -5,29 +5,29 @@ use RectorLaravel\Set\LaravelSetList;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
-$dir = '/var/www/html/app/';
-$dir_rector = '/var/www/html/tools/rector';
-$utils_rector = '/utils/rector/tests/Rector/';
-$files = [];
-$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
-foreach ($iterator as $file) {
-    // if ($file->isFile() && $file->getExtension() === 'php') {
-        $files[] = $file->getPathname();
-    // }
-}
+// $dir = '/var/www/html/app/';
+// $dir_rector = '/var/www/html/tools/rector';
+// $utils_rector = '/utils/rector/tests/Rector/';
+// $files = [];
+// $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+// foreach ($iterator as $file) {
+//     // if ($file->isFile() && $file->getExtension() === 'php') {
+//         $files[] = $file->getPathname();
+//     // }
+// }
 return RectorConfig::configure()
     ->withSets([
         LaravelSetList::LARAVEL_100
     ])
     // ->withImportNames() # use SHORT NAME , not use FQN 
     // ->withImportNames(importShortClasses: false) # use FQN , no use SHORT NAME
-    ->withSkip([
-        '/var/www/html/app/Services',
-        '/var/www/html/app/Rules/SentAtDifference.php',
-        // or use fnmatch
-        '/var/www/html/app/*/Tests/*',
-        // SimplifyIfReturnBoolRector::class,
-    ])
+    // ->withSkip([
+    //     '/var/www/html/app/Services',
+    //     '/var/www/html/app/Rules/SentAtDifference.php',
+    //     // or use fnmatch
+    //     '/var/www/html/app/*/Tests/*',
+    //     // SimplifyIfReturnBoolRector::class,
+    // ])
     ->withSkip([
         SimplifyIfReturnBoolRector::class,
     ])
