f1f6178cd5f83b17654fe02a9e80f3d53d56e238
unknown <manh.nguyen@dac-datatech.vn>
2024-05-09 12:01:11 +0700

    update rector.php

--- contents/tools/rector/rector.php
+++ contents/tools/rector/rector.php
@@ -1,10 +1,13 @@
 <?php
 
+use Rector\CodeQuality\Rector\LogicalAnd\AndAssignsToSeparateLinesRector;
 use Rector\Config\RectorConfig; 
 use RectorLaravel\Set\LaravelSetList;
 use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
+use Rector\Set\ValueObject\DowngradeLevelSetList;
+use Utils\Rector\Rector\RuleAddCommentRector;
 // $dir = '/var/www/html/app/';
 // $dir_rector = '/var/www/html/tools/rector';
 // $utils_rector = '/utils/rector/tests/Rector/';
@@ -16,9 +19,10 @@ use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
 //     // }
 // }
 return RectorConfig::configure()
-    ->withSets([
-        LaravelSetList::LARAVEL_100
-    ])
+    // ->withSets([
+        // LaravelSetList::LARAVEL_100
+        // DowngradeLevelSetList::DOWN_TO_PHP_72 # rector downgrade 
+    // ])
     // ->withImportNames() # use SHORT NAME , not use FQN 
     // ->withImportNames(importShortClasses: false) # use FQN , no use SHORT NAME
     // ->withSkip([
@@ -28,9 +32,9 @@ return RectorConfig::configure()
     //     '/var/www/html/app/*/Tests/*',
     //     // SimplifyIfReturnBoolRector::class,
     // ])
-    ->withSkip([
-        SimplifyIfReturnBoolRector::class,
-    ])
+    // ->withSkip([
+    //     SimplifyIfReturnBoolRector::class,
+    // ])
     // ->withSkip([
     //     SimplifyIfReturnBoolRector::class => [
     //         '/var/www/html/app/Services',
@@ -42,15 +46,15 @@ return RectorConfig::configure()
     //     $dir_rector . $utils_rector . 'RuleABCRector/config/configured_rule.php',
     //     $dir_rector . $utils_rector . 'RuleAddCommentRector/config/configured_rule.php'
     // ])
-    ->withAttributesSets(symfony: true, doctrine: true)
-    ->withPhpSets(php84: true)
+    // ->withAttributesSets(symfony: true, doctrine: true)
+    // ->withPhpSets(php84: true)
     ->withRules([
-        TypedPropertyFromStrictConstructorRector::class
+        RuleAddCommentRector::class
     ])
     // ->withPaths($files)
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-    )
+    // ->withPreparedSets(
+    //     deadCode: true,
+    //     codeQuality: false,
+    // )
     ->withFileExtensions(['php', 'phtml', 'js', 'blade.php']);
 
